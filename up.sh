688b13e147c09435ab0d77d28aecc1634dcbb13b
MuhammadRestu999 <muhammadrestu1108@gmail.com>
2022-12-08 14:20:49 +0700

    :v

--- up.sh
+++ up.sh
@@ -1,4 +1,11 @@
+msg="Update from up.sh"
+if [[ "$@" ]]; then
+    msg="$@"
+fi
+
 git add .
-git commit -m "Update from up.sh"
+git commit -m "$msg"
 git branch -M main
 git push -u origin main --force
+
+unset msg

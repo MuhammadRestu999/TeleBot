643746e59b44e5ad1c2220c12e2eeab05a198813
Davian <88314302+carlxjoe@users.noreply.github.com>
2022-12-12 12:30:16 +0700

    Update e-ai.js

--- cmd/e-ai.js
+++ cmd/e-ai.js
@@ -27,7 +27,7 @@ A:`
 let res = await qna(text)
 await ctx.reply(res)
   },
-  tags: "searching", "edukasi",
+  tags: "edukasi",
   help: ["ai"],
   desc: "Openai Cari Sesuatu dengan lebih mudah"
 }

6ab8656daa98fdc411f862f773956ca0697069f0
Davian <88314302+carlxjoe@users.noreply.github.com>
2022-12-12 13:47:23 +0700

    Support all System

---
+++ cmd/play.js
@@ -0,0 +1,27 @@
+const { youtubeSearch } = require("@bochilteam/scraper")
+
+module.exports = {
+  start: async function(ctx, { text }) {
+    if(!text) return ctx.reply("Cara penggunaan : /play Judul lagu\n\nContoh penggunaan : /play Hanya Rindu")
+    await ctx.reply("⏳| Silahkan tunggu")
+    let vid = (await youtubeSearch(text)).video[0]
+  if (!vid) return ctx.reply('Video/Audio Tidak ditemukan')
+  let { title, description, videoId, durationH, viewH, publishedTime } = vid
+  const url = 'https://www.youtube.com/watch?v=' + videoId
+  const { thumbnail } = await youtubedl(url).catch(async _ => await youtubedlv2(url)).catch(async _ => await youtubedlv3(url))
+
+    //let res = (await yts(text)).all.find(v => v.type == "video" && v.seconds <= 630)
+    let markup = Markup.inlineKeyboard([
+      Markup.button.callback("Audio", "yta " + url),
+      Markup.button.callback("Cancel", "c"),
+      Markup.button.callback("Video", "ytv " + url)
+    ])
+    ctx.replyWithPhoto(thumbnail, {
+      "caption": `🎵Title : ${title}\n🕒Upload date : ${publishedTime}\n👀Viewer : ${viewH}\n⌛Duration : ${durationH}\n📄Description : ${description}`,
+      ...markup
+    })
+  },
+  tags: "downloader",
+  help: ["play"],
+  desc: "Play audio/video from youtube"
+}

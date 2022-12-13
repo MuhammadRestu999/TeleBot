dd8f5162c9ac9c157b48bcb511afae550419af64
Davian <88314302+carlxjoe@users.noreply.github.com>
2022-12-13 16:31:12 +0700

    Ubah dikir

--- cmd/e-ai.js
+++ cmd/e-ai.js
@@ -1,29 +1,8 @@
-const { Configuration, OpenAIApi } = require("openai")
+const { qna } = require("../lib/openai.js")
 
 module.exports = {
   start: async function(ctx, { text }) {
 if (!text) return ctx.reply("Apa yg ingin dicari")
-
-async function qna(txt) {
-  const configuration = new Configuration({
-    apiKey: "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",
-  });
-  const openai = new OpenAIApi(configuration);
-  let text = `Q: ${txt}
-A:`
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: text,
-    temperature: 0.9,
-    max_tokens: 800,
-    top_p: 1,
-    frequency_penalty: 0.0,
-    presence_penalty: 0.0,
-    stop: ["A:"],
-  });
-  return await response.data.choices[0].text
-}
-
 let res = await qna(text)
 await ctx.reply(res)
   },

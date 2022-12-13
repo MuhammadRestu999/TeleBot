aa7799a7bd46d840484ceea180e5ba9a0d7390ee
Davian <88314302+carlxjoe@users.noreply.github.com>
2022-12-13 16:33:49 +0700

    Penambahan Soundcloud dl

---
+++ cmd/soundcloud.js
@@ -0,0 +1,17 @@
+const {scdl } = require("../lib/scrape.js")
+  
+
+module.exports = {
+  start: async function(ctx, { text }) {
+    let args = text.split(" ")
+  if (!args[0]) return ctx.reply("masukkan link")
+  let res = await scdl(args[0])
+  const { thumb, link, title} = res
+ctx.replyWithPhoto(thumb, { caption: `Judul: ${title}\n\nSedang mengirim audio...` }).then(function(msg) {
+      ctx.replyWithAudio(link, { filename: title + ".mp3" }, { reply_to_message_id: msg.message_id })
+    })
+  },
+  tags: "downloader",
+  help: ["scdl", "soundcloud"],
+  desc: "download audio from SoundCloud"
+}

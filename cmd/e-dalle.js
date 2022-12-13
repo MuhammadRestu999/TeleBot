5fc4db30161a31f5537b3711abaeffff7e1d117d
Davian <88314302+carlxjoe@users.noreply.github.com>
2022-12-13 16:30:23 +0700

    Fix

--- cmd/e-dalle.js
+++ cmd/e-dalle.js
@@ -1,24 +1,11 @@
-const { Configuration, OpenAIApi } = require("openai")
+const { dalle } = require("../lib/openai.js")
 
 module.exports = {
   start: async function(ctx, { text }) {
 if (!text) return ctx.reply("Apa yg ingin dicari")
 
-async function dalle(text) {
-  const configuration = new Configuration({
-    apiKey: "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",
-  });
-  const openai = new OpenAIApi(configuration);
-  const response = await openai.createImage({
-    prompt: text,
-    n: 1,
-    size: "1024x1024",
-  });
-  return response.data.data[0].url
-}
-
 let res = await dalle(text)
-await ctx.replyWithPhoto({ url: res, caption: `Asep🤖 Selesai Membuatkan Gambar Yg Anda inginkan!` })
+await ctx.replyWithPhoto({ url: res, caption: `Selesai Membuatkan Gambar Yg Anda inginkan!` })
   },
   tags: "searching",
   help: ["dalle"],

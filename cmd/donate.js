16ab1c7814aae5a4b7be0930d73fa045f0f68425
Davian <88314302+carlxjoe@users.noreply.github.com>
2023-01-03 18:01:10 +0700

    Update donate.js

--- cmd/donate.js
+++ cmd/donate.js
@@ -2,14 +2,14 @@ module.exports = {
   start: async function(ctx) {
     let str = `
 <pre>╭─「 Donasi • Pulsa 」
-│ • Im3 : </pre><pre>085783417029</pre>
+│ • Telkom : </pre><pre>082328303332</pre>
 <pre>╰────
 
 ╭─「 Donasi • Non Pulsa 」
-│ • Dana    : </pre><pre>085783417029</pre><pre>
-│ • Gopay   : </pre><pre>085783417029</pre><pre>
-│ • OVO     : </pre><pre>085783417029</pre><pre>
-│ • Saweria : </pre><a href="https://saweria.co/MuhammadRestu">https://saweria.co/MuhammadRestu</a><pre>
+│ • Dana    : </pre><pre>082328303332</pre><pre>
+│ • Gopay   : </pre><pre>None</pre><pre>
+│ • OVO     : </pre><pre>082328303332</pre><pre>
+│ • Saweria : </pre><a href="https://saweria.co/Sxzy">https://saweria.co/Sxzy</a><pre>
 ╰────</pre>
 `.trim()
     ctx.replyWithHTML(str, { disable_web_page_preview: true })
